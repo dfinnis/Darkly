19a1070e1ef369a49a4069f5e7713e90d6dba865
Drew Finnis <drewfinnis@gmail.com>
2020-10-27 13:22:27 +0100

    refacto/clean up flag.html after running script

--- HEADER_SPOOF/Ressources/find_flag.sh
+++ HEADER_SPOOF/Ressources/find_flag.sh
@@ -3,5 +3,7 @@ curl -s \
 -H 'User-Agent: ft_bornToSec' \
 'http://192.168.0.15/?page=e43ad1fdc54babe674da7c7b8f0127bde61de3fbe01def7d00f151c2fcca6d1c' \
 > flag.html
-cat flag.html | grep "flag" | head -c 116 | tail -c 78
+cat flag.html | grep "flag" \
+| head -c 116 | tail -c 78
 echo
+rm flag.html
